020d1dc086dac9db19aae0b232e48b1220d53651
Dan-McGrath <mcgrathd3254@yahoo.com>
2023-05-11 01:32:20 -0400

    create AequorFactory function

--- main.js
+++ main.js
@@ -13,6 +13,31 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+function pAequorFactory(id, dnaStrand) {
+  return {
+    'specimenNum': id,
+    'dna': [dnaStrand],
+
+    'mutate': function () {
+      const dnaBases = ['A', 'T', 'C', 'G'];
+      let randIdx = [Math.floor(Math.random()* 15)];
+      let randDna = this.dna[0][randIdx];
+      let newDna = dnaBases[Math.floor(Math.random() * 4)];
+
+      if (randDna !== newDna) {
+        this.dna[0][randIdx] = newDna;
+      } else {
+        this.mutate(); // if new and old dna match reruns function
+      }
+
+      return this.dna;
+    }
+
+  }
+}
+
+
+test = pAequorFactory(1, mockUpStrand());
 
 
 
